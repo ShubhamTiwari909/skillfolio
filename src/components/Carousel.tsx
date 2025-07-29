c5bb5d328ba05d9470750d97679490e6f99cb9f8
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2025-07-29 16:10:26 +0530

    fix: added images, iframe codepen and youtube iframe

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,5 +1,3 @@
-import * as React from "react"
-
 import { Card, CardContent } from "@/components/ui/card"
 import {
   Carousel,
@@ -8,33 +6,34 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import Image from "./Image"
 
 export function CarouselSize() {
   return (
     <div className="mb-10 scroll-mt-18 lg:scroll-mt-20" id="carousel">
-        <h2 className="text-3xl font-semibold mb-5">Carousel Demo</h2>
-        <Carousel
+      <h2 className="text-3xl font-semibold mb-5">Carousel Demo</h2>
+      <Carousel
         opts={{
-            align: "start",
+          align: "start",
         }}
         className="w-full max-w-full"
-        >
+      >
         <CarouselContent>
-            {Array.from({ length: 5 }).map((_, index) => (
+          {Array.from({ length: 10 }).map((_, index) => (
             <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-                <div className="p-1">
+              <div className="p-1">
                 <Card>
-                    <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-3xl font-semibold">{index + 1}</span>
-                    </CardContent>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <Image index={index} />
+                  </CardContent>
                 </Card>
-                </div>
+              </div>
             </CarouselItem>
-            ))}
+          ))}
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
-        </Carousel>
+      </Carousel>
     </div>
   )
 }
---
+++ src/components/Image.tsx
@@ -0,0 +1,14 @@
+import { Loader2 } from 'lucide-react'
+import { useState } from 'react'
+
+const Image = ({ index = 0 }) => {
+    const [loading, setLoading] = useState(true)
+    return (
+        <div className='relative'>
+            {loading ? <Loader2 className='size-10 lg:size-14 animate-spin absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' /> : null}
+            <img src={`https://picsum.photos/seed/${index}/1200/1200`} alt={`Image ${index + 1}`} width={800} height={800} loading="lazy" onLoad={() => setLoading(false)} className={`transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"}`} />
+        </div>
+    )
+}
+
+export default Image
