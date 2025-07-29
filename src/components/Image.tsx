4829d713373dbdfaa48372b7cb8ac6215c0f0e7b
Shubham-Tiwari-Anywhere <shubham.prem@anywhere.co>
2025-07-29 16:24:43 +0530

    used webp for images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -6,7 +6,7 @@ const Image = ({ index = 0 }) => {
     return (
         <div className='relative'>
             {loading ? <Loader2 className='size-10 lg:size-14 animate-spin absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' /> : null}
-            <img src={`https://picsum.photos/seed/${index}/1200/1200`} alt={`Image ${index + 1}`} width={800} height={800} loading="lazy" onLoad={() => setLoading(false)} className={`transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"}`} />
+            <img src={`https://picsum.photos/seed/${index}/800/800.webp`} alt={`Image ${index + 1}`} width={800} height={800} loading="lazy" onLoad={() => setLoading(false)} className={`transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"}`} />
         </div>
     )
 }
